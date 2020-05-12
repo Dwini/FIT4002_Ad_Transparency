e45477b9f9684f9fe0a11a77fa1357ef61c4a97c
Ashman <ashmankumar@outlook.com>
2020-05-12 16:30:55 +1000

    added function to retreive rev content ads

--- google_adsense_scrape.py
+++ google_adsense_scrape.py
@@ -1,29 +1,56 @@
-from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
 from time import sleep
 
 webdriver = "chromedriver.exe"
 
+
+
+#given a selenium driver retrieves a list of google ads which appear on the page
+def getGoogleAds(driver):
+
+
+    # google ads appear in iframes labelled as such
+    iframes = driver.find_elements_by_xpath("//iframe[@data-google-container-id]")
+
+    screenshots = []
+    i = 0
+    for iframe in iframes:
+        try:
+            screenshotName = 'adScreenshots/' + str(i) + '.png'
+            iframe.screenshot(screenshotName)
+            i = i + 1
+
+            screenshots.append(iframe.screenshot_as_base64)
+        except:
+            print('one or more screenshots failed')
+
+    return()
+
+def getRevContentAds(driver):
+
+    # rev content ads appear as
+    #revContent = driver.find_elements_by_xpath("//div[@id=rc-row-container]")
+
+    revItems = driver.find_elements_by_class_name('rc-item')
+    i = 0
+    for item in revItems:
+
+        image = item.find_element_by_class_name('rc-photo-container')
+        text = item.text
+
+        screenshotName = 'adScreenshots/' + str(i) + '.png'
+        image.screenshot(screenshotName)
+        print(image.screenshot_as_base64)
+        i = i + 1
+
+
+
 driver = Chrome(webdriver)
 
-url = "https://www.breitbart.com/"
+url = "https://www.washingtonexaminer.com/"
 driver.get(url)
 
 
-sleep(30)
+sleep(45)
 
-#google ads appear in iframes labelled as such
-iframes = driver.find_elements_by_xpath("//iframe[@data-google-container-id]")
-
-
-i = 0
-
-for iframe in iframes:
-    try:
-        screenshotName = '/adScreenshots/' + str(i) + '.png'
-        iframe.screenshot(screenshotName)
-        i = i + 1
-        print('saved')
-        print(iframe.screenshot_as_base64)
-    except:
-        print('one or more screenshots failed')
+getGoogleAds(driver)
