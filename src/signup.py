dfdb4cafbc8a385eaf34d8c9ec34ec65ba739b4b
Aiden <Dwini@users.noreply.github.com>
2020-05-25 13:53:17 +1000

    Fixed sign up entry
    Added two different options to fill out the text boxes in the creation of an account

--- src/signup.py
+++ src/signup.py
@@ -2,63 +2,81 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-EMAIL_ID = "john_doe_1998_14@gmail.com"
+EMAIL_ID = "john_doe_1998_14"
 
 def slow_typing(element, text):
    for character in text:
       element.send_keys(character)
       time.sleep(0.3)
 
+def tab_entry(browser):
+    actions = ActionChains(browser)
+    firstname = browser.find_element_by_id('firstName')
+    firstname.click()
+    actions = actions.send_keys('John')
+    actions = actions.send_keys(Keys.TAB)
+    actions = actions.send_keys('Doed')
+    actions = actions.send_keys(Keys.TAB)
+    actions = actions.send_keys('John1234Doed')
+    actions = actions.send_keys(Keys.TAB)
+    actions = actions.send_keys(Keys.TAB)
+    actions = actions.send_keys('hi1234()')
+    actions = actions.send_keys(Keys.TAB)
+    actions = actions.send_keys('hi1234()')
+    actions.perform()
+
+def find_entry(browser):
+    # Fill user's full name
+    firstname = browser.find_element_by_id('firstName')
+    lastname = browser.find_element_by_id('lastName')
+    # username.send_keys('John Doe')
+    slow_typing(firstname, 'John')
+    slow_typing(lastname, 'Doe')
+
+    time.sleep(5)
+    # Fill user's email ID
+    username = browser.find_element_by_id('username')
+    slow_typing(username, EMAIL_ID)
+
+    time.sleep(5)
+    # Fill user's password
+    # Had to use xpath for this as there is no id on the actual textbox
+    password = browser.find_elements_by_xpath('//*[@id="passwd"]/div[1]/div/div[1]/input')
+    slow_typing(password, 'hi1234()')
+
+    time.sleep(5)
+
+    # Fill user's password
+    password = browser.find_element_by_xpath('//*[@id="confirm-passwd"]/div[1]/div/div[1]/input')
+    slow_typing(password, 'hi1234()')
+
+    time.sleep(5)
+
 # Visit chrome://version/ and copy profile path in place of '<chrome user profile>'
-options = ChromeOptions().add_argument("--user-data-dir=<chrome user profile>")
+"""options = ChromeOptions().add_argument("--user-data-dir=<chrome user profile>")
 
 browser = Chrome("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", chrome_options=options)
-browser.get('http://gmail.com')
+"""
+options = webdriver.ChromeOptions()
+options.add_argument(r"C:\Users\aiden\AppData\Local\Google\Chrome\User Data")
+browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+browser.get('https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Fwww.google.com%2Fwebhp%3Fhl%3Den%26sa%3DX%26ved%3D0ahUKEwjbldiThc7pAhVQwzgGHbC3Bw0QPAgH&hl=en&dsh=S-1458628831%3A1590376291910546&gmb=exp&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp')
 
-time.sleep(5)
-
+time.sleep(10)
 # to accept cookie notification so that it doesn't interfere
 """cookie_cta = browser.find_element_by_id('accept-cookie-notification')
 cookie_cta.click()"""
-
-# Navigate to Signup Page
-"""button = browser.find_element_by_id('signupModalButton')"""
-button = browser.find_elements_by_xpath('//*[@id="ow284"]/div[2]')
-button.click()
-
-time.sleep(5)
-
-# Fill user's full name
-username = browser.find_element_by_id('user_fudll_name')
-# username.send_keys('John Doe')
-slow_typing(username, 'John Doe')
-
-time.sleep(5)
-# Fill user's email ID
-email = browser.find_element_by_id('user_email_login')
-slow_typing(email, EMAIL_ID)
-
-time.sleep(5)
-# Fill user's password
-password = browser.find_element_by_id('user_password')
-
-# Reads password from a text file because
-# it's silly to save the password in a script.
-with open('password.txt', 'r') as myfile:
-       Password = myfile.read().replace('\n', '')
-slow_typing(password, Password)
-
-time.sleep(5)
-# click on Terms and Conditions
-toc = browser.find_element_by_name('terms_and_conditions')
-toc.click()
-
-# click on signup page
-signupbutton = browser.find_element_by_id('user_submit')
-signupbutton.click()
+tab_entry(browser)
+time.sleep(10)
+# click on next
+next = browser.find_element_by_id('accountDetailsNext')
+next.click()
 
 time.sleep(20)
+
+
 
 browser.close()
