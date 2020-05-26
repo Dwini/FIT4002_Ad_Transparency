537259ebb51a6879386993eeb0123aa5deca481e
Aiden <Dwini@users.noreply.github.com>
2020-05-26 18:10:23 +1000

    Created auto generators
    Created auto generators for first and last name, username, birthday and year and gender.
    All that is left is a password generator and birth month generator.

--- src/signup.py
+++ src/signup.py
@@ -4,55 +4,55 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-
-EMAIL_ID = "john_doe_1998_14"
+import names
+import random
 
 def slow_typing(element, text):
    for character in text:
       element.send_keys(character)
       time.sleep(0.3)
 
-def tab_entry(browser):
+def tab_entry(browser, strFirstname, strLastname, strUsername, strPassword):
     actions_create = ActionChains(browser)
     firstname = browser.find_element_by_id('firstName')
     firstname.click()
-    actions_create = actions_create.send_keys('John')
+    actions_create = actions_create.send_keys(strFirstname)
     actions_create = actions_create.send_keys(Keys.TAB)
-    actions_create = actions_create.send_keys('Doed')
+    actions_create = actions_create.send_keys(strLastname)
     actions_create = actions_create.send_keys(Keys.TAB)
-    actions_create = actions_create.send_keys('John1234Doed')
+    actions_create = actions_create.send_keys(strUsername)
     actions_create = actions_create.send_keys(Keys.TAB)
     actions_create = actions_create.send_keys(Keys.TAB)
-    actions_create = actions_create.send_keys('hi1234()')
+    actions_create = actions_create.send_keys(strPassword)
     actions_create = actions_create.send_keys(Keys.TAB)
-    actions_create = actions_create.send_keys('hi1234()')
+    actions_create = actions_create.send_keys(strPassword)
     actions_create.perform()
 
 
-def find_entry(browser):
+def find_entry(browser, strFirstname, strLastname, strUsername, strPassword):
     # Fill user's full name
     firstname = browser.find_element_by_id('firstName')
     lastname = browser.find_element_by_id('lastName')
     # username.send_keys('John Doe')
-    slow_typing(firstname, 'John')
-    slow_typing(lastname, 'Doe')
+    slow_typing(firstname, strFirstname)
+    slow_typing(lastname, strLastname)
 
     time.sleep(5)
     # Fill user's email ID
     username = browser.find_element_by_id('username')
-    slow_typing(username, EMAIL_ID)
+    slow_typing(username, strUsername)
 
     time.sleep(5)
     # Fill user's password
     # Had to use xpath for this as there is no id on the actual textbox
     password = browser.find_elements_by_xpath('//*[@id="passwd"]/div[1]/div/div[1]/input')
-    slow_typing(password, 'hi1234()')
+    slow_typing(password, strPassword)
 
     time.sleep(5)
 
     # Fill user's password
     password = browser.find_element_by_xpath('//*[@id="confirm-passwd"]/div[1]/div/div[1]/input')
-    slow_typing(password, 'hi1234()')
+    slow_typing(password, strPassword)
 
     time.sleep(5)
 
@@ -66,19 +66,19 @@ def personal_next(broswer):
     next = browser.find_element_by_id('personalDetailsNext')
     next.click()
 
-def finalise(browser):
+def finalise(browser, day, month, year, gender):
     actions_finalise = ActionChains(browser)
     phone = browser.find_element_by_id('phoneNumberId')
     phone.click()
     actions_finalise = actions_finalise.send_keys(Keys.TAB)
     actions_finalise = actions_finalise.send_keys(Keys.TAB)
-    actions_finalise = actions_finalise.send_keys('12')
+    actions_finalise = actions_finalise.send_keys(day)
     actions_finalise = actions_finalise.send_keys(Keys.TAB)
-    actions_finalise = actions_finalise.send_keys('s')
+    actions_finalise = actions_finalise.send_keys(month)
     actions_finalise = actions_finalise.send_keys(Keys.TAB)
-    actions_finalise = actions_finalise.send_keys('1997')
+    actions_finalise = actions_finalise.send_keys(year)
     actions_finalise = actions_finalise.send_keys(Keys.TAB)
-    actions_finalise = actions_finalise.send_keys('m')
+    actions_finalise = actions_finalise.send_keys(gender)
     actions_finalise.perform()
 
 # Visit chrome://version/ and copy profile path in place of '<chrome user profile>'
@@ -95,9 +95,18 @@ time.sleep(10)
 # to accept cookie notification so that it doesn't interfere
 """cookie_cta = browser.find_element_by_id('accept-cookie-notification')
 cookie_cta.click()"""
-tab_entry(browser)
+
+gender = 'female'
+if random.randint(0,1):
+    gender = 'male'
+strFirstname = names.get_first_name(gender)
+strLastname = names.get_last_name(gender)
+randomInt = '1234'
+strUsername = strFirstname + '.' + strLastname + "." + randomInt
+strPassword = "Hi1234()"
+tab_entry(browser, strFirstname, strLastname, strUsername, strPassword)
 account_next(browser)
-finalise(browser)
+finalise(browser, random.randint(0, 28), 's', random.randint(1940, 2004), gender[0])
 personal_next(browser)
 
 time.sleep(20)
