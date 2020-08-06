b8e5dffac32b4d9b62987bf75dafc3b4c6e306ff
Aiden <Dwini@users.noreply.github.com>
2020-08-06 16:14:29 +1000

    Added option to create bots

--- src/app.py
+++ src/app.py
@@ -6,7 +6,9 @@ from database import Database
 import proxy
 import webscraper
 import config_driver
-
+from bot import bot
+from webscraper import websracper
+from signup import botCreator
 
 def main():
     container_build = False
@@ -22,41 +24,43 @@ def main():
     
     db = Database()
 
-    bots = db.fetch_all_items('Bots')
+    creating = False
+    if creating:
+        newBot = botcreator()
+    else:
+        bots = db.fetch_all_items('Bots')
 
-    for bot in bots:
-        # todo: remove to use all bots. this is only for testing
-        if bot['username'] != "jw1083888":
-            continue
+        for bot in bots:
+            # todo: remove to use all bots. this is only for testing
+            if bot['username'] != "jw1083888":
+                continue
 
-        print('using bot: ' + bot['username'])
+            print('using bot: ' + bot['username'])
 
-        # define location of bot
-        pos = None
-        if 'location' in bot:
-            pos = {
-                'lat': float(bot['location']['latitude']),
-                'lon': float(bot['location']['longitude'])
-            }
-        else:
-            # default pos to random position
-            pos = { 'lat': uniform(-90, 90), 'lon': uniform(-180, 180) }
+            # define location of bot
+            pos = None
+            if 'location' in bot:
+                pos = {
+                    'lat': float(bot['location']['latitude']),
+                    'lon': float(bot['location']['longitude'])
+                }
+            else:
+                # default pos to random position
+                pos = { 'lat': uniform(-90, 90), 'lon': uniform(-180, 180) }
 
-        # use this to setup driver with proxy that is closest
-        # proxyIP = proxy.get_closest_proxy(pos)
-        # session = config_driver.setup_driver(proxyIP)
-        # proxy.ip_check(session)
+            # use this to setup driver with proxy that is closest
+            # proxyIP = proxy.get_closest_proxy(pos)
+            # session = config_driver.setup_driver(proxyIP)
+            # proxy.ip_check(session)
 
-        # use this to setup driver without proxy
-        session = config_driver.setup_driver()
-        
-        # change location
-        config_driver.set_location(session, pos)
+            # use this to setup driver without proxy
+            session = config_driver.setup_driver()
 
-        # start scraping
-        webscraper.login(bot, session)
-        webscraper.setup_profile(bot, session, db)
-        webscraper.scrape_google_ads(bot, session, db)
+            # change location
+            config_driver.set_location(session, pos)
+
+            # start scraping
+            webscraper = websracper(webdriver, bot)
 
     # close display if in container.
     if container_build == True:
