4c1233e6de8b35a5e4eeb854fca1b2c98d665387
honeybadger26 <honeybadger26@gmail.com>
2020-10-12 16:17:36 +1100

    -must provide date with ads query -html to file if chars exceed 100

--- db/routes/ads.js
+++ db/routes/ads.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const moment = require('moment-timezone');
 const multer = require('multer');
 const fs = require('fs');
+const util = require('util');
 
 const { DATETIME_FORMAT } = require('../config')
 const { accessKeyId, secretAccessKey, region, 
@@ -12,6 +13,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 var s3 = new AWS.S3();
 
 const upload = multer({ dest: '/tmp/' });
+const writeFile = util.promisify(fs.writeFile);
 
 function uuidv4() {
     /**
@@ -28,23 +30,41 @@ module.exports = app => {
     app.route('/ads')
         .get(function(req, res, next) {
             /** 
-             * Fetch all Ads from db.
-             * @param query.bot - Username of bot to filter by
-             * TODO: Return ads for the given day only.
-             * See: https://stackoverflow.com/questions/43793888/how-to-make-search-using-contains-with-dynamodb
-             */
-            var params = { TableName: 'Ads' };
-
-            const { bot } = req.query;
+            * Fetch all Ads from db.
+            * @param query.date - Date of ads to retrieve. This should be a substring
+            *      the format specified as DATETIME_FORMAT in config.js. For example to get
+            *      ads for a specific set to: MM-YY or for a specific day use: DD-MM-YYYY
+            * @param query.bot - (optional) Username of bot to filter by
+            * TODO: Return ads for the given day only.
+            * See: https://stackoverflow.com/questions/43793888/how-to-make-search-using-contains-with-dynamodb
+            */
+            const { bot, date } = req.query;
+            
+            if (!date) return res.status(400).send('Missing date field');
+            
+            var FilterExpression = 'contains(#date, :date)',
+            ExpressionAttributeNames = { '#date': 'datetime' },
+            ExpressionAttributeValues = { ':date': date };
+            
             if (bot) {
-                params = { 
-                    ...params, 
-                    FilterExpression: '#b = :b',
-                    ExpressionAttributeNames: { '#b': 'bot' },
-                    ExpressionAttributeValues: { ':b': bot } 
+                FilterExpression += ' AND #b = :b';
+                ExpressionAttributeNames = { 
+                    ...ExpressionAttributeNames, 
+                    '#b': 'bot' 
+                };
+                ExpressionAttributeValues = { 
+                    ...ExpressionAttributeValues, 
+                    ':b': bot 
                 };
             };
-
+            
+            const params = {
+                TableName: 'Ads',
+                FilterExpression,
+                ExpressionAttributeNames,
+                ExpressionAttributeValues
+            };
+            
             docClient.scan(params, function(err, data) {
                 if (err) return next(err);
 
@@ -57,7 +77,7 @@ module.exports = app => {
                 res.send(resp);
             });
         })
-        .post(upload.single('file'), function(req, res, next) {
+        .post(upload.single('file'), async function(req, res, next) {
             /**
              * Creates a new Ad
              * @param body.bot       - Username of bot that captured ad
@@ -74,44 +94,98 @@ module.exports = app => {
             // Allowed fields
             const { bot, link, headline, html, base64 } = req.body;
 
+            console.log(req.body);
+
             // Required fields
             if (!bot || !link || !headline) {
                 return res.status(400).send('Missing required field(s)');
             }
 
-            const saveAd = function(params) {
-                docClient.put(params, function(err) {
-                    if (err) return next(err);
-                    res.sendStatus(200);
-                });
-            };
-
-            var dbParams = { TableName: 'Ads' };
             var Item = { 
-                bot, link, headline, html, base64,
+                bot, link, headline, base64,
                 id: uuidv4(),
                 datetime: moment(new Date()).format(DATETIME_FORMAT)
             };
+            var filename, filepath, s3Params, data;
+            
+            // Upload html as file if over 100 characters
+            if (html.length > 100) {
+                filename = Date.now() + '.html';
+                filepath = '/tmp/' + filename;
+                
+                try {
+                    await writeFile(filepath, html);
+                } catch (err) {
+                    return next(err);
+                }
+                
+                s3Params = {
+                    Bucket: bucket,
+                    Body: fs.createReadStream(filepath),
+                    Key: 'html/' + filename
+                };
 
-            if (!file) {
-                dbParams.Item = Item
-                return saveAd(dbParams);
+                try {
+                    data = await s3.upload(s3Params).promise();
+                } catch (err) {
+                    fs.unlink(filepath, () => {});
+                    return next(err);
+                }
+                
+                fs.unlink(filepath, () => {});
+                Item.html = data.Location; 
+            } else {
+                Item.html = html;
+            }
+            
+            // Upload any file sent with this request
+            if (file) {
+                filename = Date.now() + '_' + file.originalname;
+                filepath = file.path;
+                s3Params = {
+                    Bucket: bucket,
+                    Body: fs.createReadStream(filepath),
+                    Key: filename
+                };
+
+                try {
+                    data = await s3.upload(s3Params).promise();
+                } catch (err) {
+                    fs.unlink(filepath, () => {});
+                    return next(err);
+                }
+
+                fs.unlink(filepath, () => {});
+                Item.file = data.Location;
             }
 
-            const filePath = file.path;
-            const s3Params = {
-                Bucket: bucket,
-                Body: fs.createReadStream(filePath),
-                Key: Date.now() + '_' + file.originalname
-            };
+            console.log(Item);
             
-            s3.upload(s3Params, function(err, data) {
-                fs.unlink(filePath, () => {});
+            const params = { TableName: 'Ads', Item };
+            docClient.put(params, function(err) {
                 if (err) return next(err);
-                
-                Item.file = data.Location;
-                dbParams.Item = Item
-                saveAd(dbParams);
+                res.sendStatus(200);
             });
+            
+            // if (!file) {
+            //     dbParams.Item = Item
+            //     return saveAd(dbParams);
+            // }
+            
+            // const filePath = file.path;
+            // const s3Params = {
+            //     Bucket: bucket,
+            //     Body: fs.createReadStream(filePath),
+            //     Key: Date.now() + '_' + file.originalname
+            // };
+            
+            // s3.upload(s3Params, function(err, data) {
+            //     fs.unlink(filePath, () => {});
+            //     if (err) return next(err);
+            
+            //     Item.file = data.Location;
+            //     dbParams.Item = Item
+            //     saveAd(dbParams);
+            // });
         });
 }
